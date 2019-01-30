a512bb9cf8df11d59f9fc4fe430038ba175ad5b5
Wissam Youssef <wissam@users.noreply.github.com>
2019-01-30 00:51:07 -0500

    Added first_setup file (in progress)

---
+++ bin/first_setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# apt first installations
+sudo apt update
+sudo apt install vim git ansible zsh curl htop tmux 
+
+# get google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# get slack.
+
+
+# configure vim
+
+
+# get oh-my-zsh/powerline fonts and configure it 
+
+
+# get bat
+
+
+# generate ssh-keys
+
+
+# install awscli and configure it 
+
+
+# install docker
+
+
+# install golang 
+
+
+# configure ssh 
+
+
+
+
