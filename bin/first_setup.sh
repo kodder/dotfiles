2f44462d27321bae01b5c38521f1c9b7d8baa61f
Wissam Youssef <wissam@users.noreply.github.com>
2020-11-05 21:59:44 +0200

    Synced dotfiles

--- bin/first_setup.sh
+++ bin/first_setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # apt first installations
-sudo apt-get update -y
-sudo apt-get install -y vim git ansible zsh curl htop python-setuptools python-pip silversearcher-ag ripgrep
+sudo apt-get update 
+sudo apt-get install -y vim git ansible zsh curl htop python-setuptools python-pip silversearcher-ag ripgrep fd-find fzf bat neovim
 
 # get google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -12,8 +12,13 @@ rm google-chrome-stable_current_amd64.deb
 # get slack.
 sudo snap install slack
 
+# Get nodejs and yarn for CoC.nvim
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list 
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt update && sudo apt install -y nodejs yarn 
+
 # configure vim
-sudo apt install vim neovim
 
 
 # get oh-my-zsh/powerline fonts and configure it 
--- init.vim
+++ init.vim
@@ -2,7 +2,10 @@ call plug#begin()
 Plug 'tpope/vim-sensible' " sets some normal standards
 Plug 'vim-airline/vim-airline' " status bar
 Plug 'vim-airline/vim-airline-themes' "status bar theme
-
+Plug 'neoclide/coc.nvim'
+Plug 'nvim-lua/popup.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-lua/telescope.nvim'
 call plug#end()
 
 
